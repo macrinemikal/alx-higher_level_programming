337f2be7acfee6323ed4b11449947e0511f475ed
macrinemikal <macrinemikal@gmail.com>
2023-08-16 07:29:50 -0700

    pythonn2

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/values pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
