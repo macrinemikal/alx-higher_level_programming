4e4fa9075ed4add5c41bf5b89a398042d692ec91
macrinemikal <macrinemikal@gmail.com>
2023-08-16 07:15:03 -0700

    new

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,5 +2,5 @@
 def print_sorted_dictionary(a_dictionary):
     list_ord = list(a_dictionary.keys())
     list_ord.sort()
-    for k in list_ord:
+    for i in list_ord:
         print("{}: {}".format(i, a_dictionary.get(i)))
